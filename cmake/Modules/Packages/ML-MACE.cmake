7d0543f3598a0cdc44960af5eec983915c97c624
Chuck Witt <witt.chuck@gmail.com>
2023-05-30 15:13:00 +0100

    tidy.

--- cmake/Modules/Packages/ML-MACE.cmake
+++ cmake/Modules/Packages/ML-MACE.cmake
@@ -5,4 +5,3 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
 
 target_link_libraries(lammps PRIVATE "${TORCH_LIBRARIES}")
 set_property(TARGET lammps PROPERTY CXX_STANDARD 14)
-add_compile_definitions(MACE_DEBUG)
